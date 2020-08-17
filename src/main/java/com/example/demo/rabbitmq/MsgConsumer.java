495a22ce970b94cb187170acc66970aed1554326
kcy5856@outlook.com <kcy5856@outlook.com>
2020-08-17 11:20:25 +0800

    监听类注入业务bean

--- src/main/java/com/example/demo/rabbitmq/MsgConsumer.java
+++ src/main/java/com/example/demo/rabbitmq/MsgConsumer.java
@@ -1,16 +1,22 @@
 package com.example.demo.rabbitmq;
 
+import com.example.demo.service.DemoService;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 @RabbitListener(queues = "${biz.user.topic}")
 public class MsgConsumer {
 
+    @Autowired
+    DemoService demoService;
+
     @RabbitHandler
     public void received(String msg){
         System.out.println("消费者消费消息了："+msg);
+        demoService.doSomeThing();
         //TODO 这里可以做异步的工作
     }
 
